eee4856cd2a166b83f76195de3e37fd823252967
hashparty1 <alanwilhelm@gmail.com>
2021-05-12 03:22:19 -0700

    CI@1620785656

--- lib/ethereum/transport.ex
+++ lib/ethereum/transport.ex
@@ -69,6 +69,8 @@ defmodule Ethereum.Transport do
     {:ok, encoded} = Jason.encode(enc)
     result = 
       __MODULE__.post!(daemon_host, encoded)
+      |> Map.get(:body)
+      |> Map.get("result")
       
     IEx.pry
     # Logger.warn "#{inspect result}"
