46f326223362529a4e3b712245c2e4189a3d8606
hashparty1 <alanwilhelm@gmail.com>
2021-05-12 03:16:51 -0700

    CI@1620785656

--- lib/ethereum/transport.ex
+++ lib/ethereum/transport.ex
@@ -12,7 +12,7 @@ defmodule Ethereum.Transport do
     {"Api-Key", "c0e72e98-cd3a-4039-81ae-2909bb1b30ef"},
     {"Api-Secret", "d525304c-9311-431b-890b-a6411dcde1f8"},
   ]
-  # plug Tesla.Middleware.JSON
+  plug Tesla.Middleware.JSON
 
   @doc false
   @spec send(method :: String.t, params :: map) :: {:ok, map} | {:error, String.t}
