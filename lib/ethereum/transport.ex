5ad0beaa9846a38fb98a11585abeded6fad58fe2
hashparty1 <alanwilhelm@gmail.com>
2021-05-11 22:06:02 -0700

    CI@1620785656

--- lib/ethereum/transport.ex
+++ lib/ethereum/transport.ex
@@ -21,6 +21,10 @@ defmodule Ethereum.Transport do
       method: method, 
       params: params, 
       jsonrpc: "2.0",
+      headers: [
+        {"Api-Key", "c0e72e98-cd3a-4039-81ae-2909bb1b30ef"},
+        {"Api-Secret", "d525304c-9311-431b-890b-a6411dcde1f8"},
+      ],
       id: 0
     }
 
