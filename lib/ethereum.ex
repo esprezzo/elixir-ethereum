31bd490f44b9da0c8ec37d60d7e805b127860944
alanwilhelm <ajwilhelm@protonmail.com>
2021-12-07 09:26:28 -0800

    ENH: extend signature of getBlock

--- lib/ethereum.ex
+++ lib/ethereum.ex
@@ -1,11 +1,11 @@
 defmodule Ethereum do
   @moduledoc """
-  This library presents a convenient interface to control a full Ethereum node from Elixir, 
-  abstracting away the need to deal with the JSON-RPC API directly. 
-  It decodes the hex responses when necessary and functions return the idiomatic {:ok, data} | {:error, reason} 
+  This library presents a convenient interface to control a full Ethereum node from Elixir,
+  abstracting away the need to deal with the JSON-RPC API directly.
+  It decodes the hex responses when necessary and functions return the idiomatic {:ok, data} | {:error, reason}
   tuples whenever possible. The goal is to cover the entire JSON-RPC API for Geth/Parity.
 
-  The main module acts as an "interface" or "facade". 
+  The main module acts as an "interface" or "facade".
   It delegates all functionality to submodules for clarity and to keep modules smaller.
   """
   alias Ethereum.Web3
@@ -22,7 +22,7 @@ defmodule Ethereum do
   defdelegate get_transaction_receipt_by_hash(hash), to: Eth
   defdelegate get_block_by_hash(hash, full_txns), to: Eth
   defdelegate get_block_by_number(number, full \\ false), to: Eth
-  defdelegate get_balance(account_hash), to: Eth
+  defdelegate get_balance(account_hash, block \\ "latest"), to: Eth
   defdelegate protocol_version(), to: Eth
   defdelegate syncing(), to: Eth
   defdelegate coinbase(), to: Eth
@@ -42,7 +42,7 @@ defmodule Ethereum do
   defdelegate get_filter_changes(hash), to: Eth
   defdelegate uninstall_filter(id), to: Eth
   defdelegate new_filter(map), to: Eth
-  
+
   ## Web3 Namespace Functions
   defdelegate client_version(), to: Web3
   defdelegate sha3(str), to: Web3
@@ -68,12 +68,12 @@ defmodule Ethereum do
   defdelegate get_average_blocktime(blocks), to: Aggregates
   defdelegate get_average_difficulty(blocks), to: Aggregates
 
-  ## Encoding + Utils 
+  ## Encoding + Utils
   defdelegate unhex(str), to: HexUtils
   defdelegate to_hex(str), to: HexUtils
   defdelegate is_valid_address?(address), to: HexUtils
   defdelegate hex_to_decimal(hex_string), to: HexUtils
-  
+
   ## ABI Functions
   defdelegate load_abi(file), to: ABI
   defdelegate reformat_abi(abi), to: ABI
@@ -86,7 +86,7 @@ defmodule Ethereum do
   defdelegate decode_abi_data(types_signature, data), to: ABI, as: :decode_data
   defdelegate decode_abi_output(abi, name, output), to: ABI, as: :decode_output
   defdelegate abi_keys_to_decimal(map, keys), to: ABI, as: :keys_to_decimal
-  
+
   # TX Pool functions
 
 end
